b6a34b847897d24e470ea9add3c34ad9cb6d30b9
democracyKim <peppertones0123@gmail.com>
2023-01-03 21:01:41 +0900

    printf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,115 +6,12 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 16:54:10 by minkim3           #+#    #+#             */
-/*   Updated: 2023/01/03 20:55:26 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/01/03 20:58:53 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_printf.h"
-
-int	ft_percent_or_other(char c)
-{
-	if (c == '%')
-		return (1);
-	else if (32 < c && c < 126)
-		return (2);
-	return (-1);
-}
-
-int ft_flag_checker(char c)
-{
-	if (c == '-' || c == '+' || c == ' ' || c == '0' || c == '#')
-		return (1);
-	return (0);
-}
-void	ft_flag_check(char c, options *string_info)
-{
-	if (c == '-')
-		string_info->flag_minus = 1;
-	else if (c == '+')
-		string_info->flag_plus = 1;
-	else if (c == ' ')
-		string_info->flag_space = 1;
-	else if (c == '0')
-		string_info->flag_zero = 1;
-	else if (c == '#')
-		string_info->flag_hash = 1;
-}
-
-void	ft_width_check(const char *format, size_t *index, options *string_info)
-{
-	int num;
-	char c;
-
-	num = 0;
-	string_info->width = -1;
-	c = *(format + *(index));
-	if ((c < 0) || (c > 9))
-		return ;
-	while ((c >= 0) && (c <= 9))
-    {
-		num *= 10;
-		num += (c - '0');
-		(*index)++;
-		c = *(format + *(index));
-    }
-	string_info->width = num;
-	// 오버플로우 또는 언더플로우의 경우에는 -1 or 0
-	// 
-}
-
-void	ft_precision_check(const char *format, size_t *index, options *string_info)
-{
-	int num;
-	char c;
-
-	num = 0;
-	string_info->precision = -1;
-	c = *(format + (*index));
-	if ((c < 0) || (c > 9))
-		return ;
-	while ((c >= 0) && (c <= 9))
-    {
-		num *= 10;
-		num += (c - '0');
-		(*index)++;
-		c = *(format + (*index));
-    }
-	string_info->precision = num;
-}
-
-int ft_type_checker(char c)
-{
-	if (c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X' || c == 'c')
-		return (1);  // 인자가 숫자
-	else if (c == '%')
-		return (2);  // % 인자 안받음
-	else if (c == 's')
-		return (3);  // s는 char *
-	else if (c == 'p')
-		return (4);  // p는 unsigned long long
-	return (0);
-}
-
-int	ft_conversion(const char *format, size_t *index, options *string_info)
-{
-	ft_memset(string_info, 0, sizeof(options));
-	while (ft_flag_checker(*(format + *(index))))  //flag check
-	{
-		ft_flag_check(*(format + *(index)), string_info);
-		(*index)++;
-	}
-	ft_width_check(format, index, string_info);  // width check
-	if (*(format + (*index)) == '.')
-		(*index)++;
-	ft_precision_check(format, index, string_info);
-	if (ft_type_checker(*(format + (*index))) == 0)  // type check
-		return (-1);
-	string_info->type = *(format + (*index));
-	// print
-	return (0);
-}
 
 int ft_apply_int(options *string_info, int value)
 {
@@ -134,12 +31,6 @@ char *ft_apply_string(options *string_info, char *value)
 char *ft_apply_pointer(options *string_info, unsigned long long value)
 {
 
-}
-
-int ft_error(void)
-{
-	write(1, "-1", 2);
-	return (-1);
 }
 
 int ft_printf(const char *format, ...)
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/27 22:13:25 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/01/03 16:36:11 by hyojocho         ###   ########.fr       */
+/*   Created: 2023/01/03 16:53:07 by minkim3           #+#    #+#             */
+/*   Updated: 2023/01/03 21:00:39 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
@@ -19,7 +20,7 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-typedef struct options_type{
+typedef struct options_type {
 	int flag_minus;
 	int flag_plus;
 	int flag_space;
@@ -29,6 +30,25 @@ typedef struct options_type{
 	int precision;
 	int type;
 } options;
+
+
+int		ft_percent_or_other(char c);
+int 	ft_flag_checker(char c);
+void	ft_flag_check(char c, options *string_info);
+void	ft_width_check(const char *format, size_t *index, options *string_info);
+void	ft_precision_check(const char *format, size_t *index, options *string_info);
+int 	ft_type_checker(char c);
+int		ft_conversion(const char *format, size_t *index, options *string_info);
+int		ft_error(void);
+int 	ft_print_backslash(const char *format, int *index);
+
+int		count_digit_number_for_hexadecimal(int decimal);
+char	*decimal_to_string(int decimal);
+char	*decimal_to_string_nosign(int decimal);
+char	*decimal_to_lowcase_hexadecimal(int decimal);
+char	*decimal_to_uppercase_hexadecimal(int decimal);
+
+
 
 
 
