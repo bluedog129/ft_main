e5efe8f9e58eec39ac662bb0f2a8b4fb94f8e127
Hyojong Choi <hyojocho@c4r6s8.42seoul.kr>
2023-01-03 18:57:56 +0900

    feat(ft_apply_digit_type) : 숫자타입 변환 구현

---
+++ printf/ft_apply_digit_type.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_apply_digit_type.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 15:15:31 by hyojocho          #+#    #+#             */
+/*   Updated: 2023/01/03 18:55:02 by hyojocho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		count_digit_number_for_hexadecimal(int decimal)
+{
+	int digit_number;
+
+	digit_number = 0;
+	while (decimal > 0)
+	{
+		decimal /= 16;
+		digit_number++;
+	}
+	return (digit_number);
+}
+
+char	*decimal_to_string(int decimal)
+{
+	char	*result;
+	
+	result = ft_itoa(decimal);
+	return (result);
+}
+
+char	*decimal_to_string_nosign(int decimal)
+{
+	char	*result;
+	
+	if (decimal < 0)
+		decimal *= -1;
+	result = ft_itoa(decimal);
+	return (result);
+}
+
+char	*decimal_to_lowcase_hexadecimal(int decimal)
+{
+	char	*result;
+	int		digit_number;
+
+	digit_number = count_digit_number_for_hexadecimal(decimal);
+	result[digit_number] = '\0';
+	digit_number--;
+	while (decimal > 0)
+	{
+		result[digit_number] = "0123456789abcdef"[decimal % 16];
+		decimal /= 16;
+		digit_number--;
+	}
+	return (result);
+}
+
+char	*decimal_to_uppercase_hexadecimal(int decimal)
+{
+	char	*result;
+	int		digit_number;
+
+	digit_number = count_digit_number_for_hexadecimal(decimal);
+	result[digit_number] = '\0';
+	digit_number--;
+	while (decimal > 0)
+	{
+		result[digit_number] = "0123456789ABCDEF"[decimal % 16];
+		decimal /= 16;
+		digit_number--;
+	}
+	return (result);
+}
