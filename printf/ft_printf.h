9d16d56bc8fce3f62e6c14306040f10f26417c1b
Hyojong Choi <hyojocho@c4r6s8.42seoul.kr>
2023-01-03 16:46:07 +0900

    printf setting

---
+++ printf/ft_printf.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 22:13:25 by hyojocho          #+#    #+#             */
+/*   Updated: 2023/01/03 16:36:11 by hyojocho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+typedef struct options_type{
+	int flag_minus;
+	int flag_plus;
+	int flag_space;
+	int flag_zero;
+	int flag_hash;
+	int width;
+	int precision;
+	int type;
+} options;
+
+
+
+#endif
