79a8c45c9219bda5a63ab5fbf0eae5afc5d4cd8f
Hyojong Choi <hyojocho@c4r7s4.42seoul.kr>
2023-01-04 19:14:49 +0900

    feat(ft_apply_flags) : apply_flags_space, plus, minus

--- printf/ft_apply_flags.c
+++ printf/ft_apply_flags.c
@@ -6,21 +6,41 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:05:42 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/01/04 14:58:43 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/01/04 19:11:19 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	apply_flag_minus(char *str)
+char	*apply_flag_minus(char *str)
 {
-	int	str_len;
+	int 	i;
+	int		buff_index;
+	char	*buff;
 
-	str_len = ft_strlen(str);
-	
+	i = 0;
+	buff_index = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			buff[buff_index] = str[i];
+			str[i] = ' ';
+			buff_index++;
+		}
+		i++;
+	}
+	buff[buff_index] = '\0';
+	i = 0;
+	while (buff[i])
+	{
+		str[i] = buff[i];
+		i++;
+	}
+	return (str);
 }
 
-void	apply_flag_plus(char *str)
+char	*apply_flag_plus(char *str)
 {
 	int		converted_int;
 	char	*converted_str;
@@ -30,3 +50,20 @@ void	apply_flag_plus(char *str)
 	return (converted_str);
 }
 
+char	*apply_flag_space(char *str)
+{
+	int		converted_int;
+	char	*positive_result;
+	int		str_len;
+	
+	str_len = ft_strlen(str);
+	converted_int = ft_itoa(str);
+	if (converted_int > 0)
+	{
+		*positive_result = ' ';
+		*(positive_result + 1) = '\0';
+		ft_strlcat(positive_result, str, str_len + 2);
+		return (positive_result);
+	}
+	return (str);
+}
