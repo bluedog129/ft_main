6d999178aa562a003c0139aca87925ec87daa434
Hyojong Choi <hyojocho@c4r7s4.42seoul.kr>
2023-01-04 15:08:39 +0900

    voidpointer_to_hexadecimal

--- printf/ft_apply_digit_type.c
+++ printf/ft_apply_digit_type.c
@@ -6,13 +6,26 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:15:31 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/01/04 14:44:56 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/01/04 15:07:54 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		count_digit_number_for_hexadecimal(int decimal)
+int		count_digit_number(int decimal)
+{
+	int digit_number;
+
+	digit_number = 0;
+	while (decimal > 0)
+	{
+		decimal /= 16;
+		digit_number++;
+	}
+	return (digit_number);
+}
+
+int		count_digit_number_ull(int decimal)
 {
 	int digit_number;
 
@@ -48,7 +61,7 @@ char	*decimal_to_lowcase_hexadecimal(int decimal)
 	char	*result;
 	int		digit_number;
 
-	digit_number = count_digit_number_for_hexadecimal(decimal);
+	digit_number = count_digit_number(decimal);
 	result[digit_number] = '\0';
 	digit_number--;
 	while (decimal > 0)
@@ -65,7 +78,7 @@ char	*decimal_to_uppercase_hexadecimal(int decimal)
 	char	*result;
 	int		digit_number;
 
-	digit_number = count_digit_number_for_hexadecimal(decimal);
+	digit_number = count_digit_number(decimal);
 	result[digit_number] = '\0';
 	digit_number--;
 	while (decimal > 0)
@@ -82,7 +95,7 @@ char	*voidpointer_to_uppercase_hexadecimal(unsigned long long decimal)
 	char	*result;
 	int		digit_number;
 
-	digit_number = count_digit_number_for_hexadecimal(decimal);
+	digit_number = count_digit_number_for_ull(decimal) + 2;
 	result[digit_number] = '\0';
 	digit_number--;
 	while (decimal > 0)
@@ -91,5 +104,8 @@ char	*voidpointer_to_uppercase_hexadecimal(unsigned long long decimal)
 		decimal /= 16;
 		digit_number--;
 	}
+	result[digit_number] = 'X';
+	digit_number--;
+	result[digit_number] = 'O'; 
 	return (result);
 }
