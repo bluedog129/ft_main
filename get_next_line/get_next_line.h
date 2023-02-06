226990912d78cbb72390656cc3d7f9259dae1e71
Hyojong Choi <bluedog129@gmail.com>
2023-02-06 22:03:50 +0900

    feat(get_next_line.h) : 사용하지 않는 함수 제거

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 21:25:05 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/02/05 21:25:07 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/02/06 22:02:51 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
-# include <fcntl.h>
 # include <string.h>
 # include <stdlib.h>
+# include <limits.h>
 
 char	*get_next_line(int fd);
 ssize_t	find_char(const char *s, int c);
