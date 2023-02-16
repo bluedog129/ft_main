ae95474e81bb83d9c7de184d33336deb13340109
Hyojong Choi <bluedog129@gmail.com>
2023-02-16 20:58:23 +0900

    delete : .vscode

--- so_long/main.c
+++ so_long/main.c
@@ -92,15 +92,26 @@
 // }
 
 
+static int validate_arg(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		print_error("arg error\n");
+		return (ERROR);
+	}
+	if (ft_strncmp(ft_strrchr(av[1], '.'),".ber", \
+		ft_strlen(ft_strrchr(av[1], '.'))) != 0)
+	{
+		print_error("arg error\n");
+		return (ERROR);
+	}
+	return (1);
+}
 
 int main(int ac, char **av)
 {
-	if (ac < 2)
-	{
-		write(1, "arg err", 1);
+	if (validate_arg(ac, av) == ERROR)
 		return (0);
-	}
 	so_long(av[1]);
-
 	return (0);
 }
