9de4a97e344968caf9c1cacd6110920e8c6b5f62
Hyojong Choi <bluedog129@gmail.com>
2023-02-21 16:17:55 +0900

    refactor(main) : main 디렉토리 위치 변경

---
+++ so_long/srcs/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 15:58:02 by hyojocho          #+#    #+#             */
+/*   Updated: 2023/02/21 16:15:43 by hyojocho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static int	validate_arg(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		print_error("arg error\n");
+		return (ERROR);
+	}
+	if (ft_strncmp(ft_strrchr(av[1], '.'), ".ber", \
+		ft_strlen(ft_strrchr(av[1], '.'))) != 0)
+	{
+		print_error("arg error\n");
+		return (ERROR);
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	if (validate_arg(ac, av) == ERROR)
+		return (0);
+	so_long(av[1]);
+	return (0);
+}
