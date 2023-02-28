f670444369c918e34fe4ef19288695bb4a58d133
Hyojong Choi <bluedog129@gmail.com>
2023-02-28 18:41:54 +0900

    feat(validat_args) : make validate_args

---
+++ push_swap/srcs/args_validate.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   args_validate.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 13:54:19 by hyojocho          #+#    #+#             */
+/*   Updated: 2023/02/28 18:32:48 by hyojocho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	validate_not_digit(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (av[i])
+	{
+		while (av[i][j])
+		{
+			if(av[i][j] == '-')
+				j++;
+			if(ft_isdigit(av[i][j]) == 0)
+				return (ERROR);
+			j++;
+		}
+		i++;
+	}
+	return (VALIDATE_PASS);
+}
+
+static int	validate_not_duplicate(char **av)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (av[i])
+	{
+		j = i + 1;
+		while (av[j])
+		{
+			if (ft_strcmp(av[i], av[j]) == 0)
+				return (ERROR);
+			j++;
+		}
+		i++;
+	}
+	return (VALIDATE_PASS);
+}
+
+static int	validate_range(char **av)
+{
+	int	i;
+
+	while (av[i])
+	{
+		if (-2147483648 > ft_atoi(av[i]) || ft_atoi(av[i]) > 2147483647)
+			return (ERROR);
+	}
+	return (VALIDATE_PASS);
+}
+
+int	validate_args(int ac, char **av)
+{
+	if (ac < 2)
+		print_error("ERROR : args_not_digit_error");
+	if (validate_not_digit(av) == ERROR)
+		print_error("ERROR : args_type_error");
+	if (validate_not_duplicate(av) == ERROR)
+		print_error("ERROR : args_duplicate_error");
+	if (validate_range(av) == ERROR)
+		print_error("ERROR : args_range_error");
+	return (VALIDATE_PASS);
+}
