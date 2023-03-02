781a081e80bcc58dab46d06e52ad245f36a675c7
Hyojong Choi <bluedog129@gmail.com>
2023-03-02 21:57:32 +0900

    test(push_swap) : commands test

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,22 +6,17 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:23:00 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/03/01 21:35:52 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/03/02 21:51:03 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_deque	*set_dequeue(char **av)
+void	put_args_in_deque(t_deque	*deque, char **av)
 {
-	t_deque	*deque;
 	int		args_num;
 	int		index;
 
-	index = 1;
-	while (av[index])
-		index++;
-	deque = create_deque(index);
 	index = 1;
 	while (av[index])
 	{
@@ -29,14 +24,32 @@ static t_deque	*set_dequeue(char **av)
 		push_back(deque, args_num);
 		index++;
 	}
+}
+
+static t_deque	*set_dequeue(char **av)
+{
+	t_deque	*deque;
+	int		index;
+
+	index = 1;
+	while (av[index])
+		index++;
+	deque = create_deque(index);
 	return (deque);
 }
 
 void	push_swap(char **av)
 {
-	t_deque	*deque;
+	t_deque	*deque_a;
+	t_deque *deque_b;
 	
-	deque = set_dequeue(av);
-	print_deque(deque);
-	free(deque);
+	deque_a = set_dequeue(av);
+	deque_b = set_dequeue(av);
+	put_args_in_deque(deque_a, av);
+	put_args_in_deque(deque_b, av);
+	print_deque(deque_a);
+	print_deque(deque_b);
+	rrr(deque_a, deque_b);
+	print_deque(deque_a);
+	print_deque(deque_b);
 }
--- push_swap/srcs/push_swap.h
+++ push_swap/srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:23:04 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/03/01 13:26:23 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/03/02 21:51:27 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
-# include "../dequeue/dequeue.h"
+# include "./dequeue/dequeue.h"
 # include <limits.h>
 
 enum e_definition
@@ -27,5 +27,16 @@ enum e_definition
 int		validate_args(int ac, char **av);
 void	print_error(char *str);
 void	push_swap(char **av);
+void	sa(t_deque	*deque);
+void	sb(t_deque *deque);
+void	ss(t_deque *deque_a, t_deque *deque_b);
+void	pa(t_deque *deque_a, t_deque *deque_b);
+void	pb(t_deque *deque_a, t_deque *deque_b);
+void	ra(t_deque *deque);
+void	rb(t_deque *deque);
+void	rr(t_deque *deque_a, t_deque *deque_b);
+void	rra(t_deque *deque);
+void	rrb(t_deque *deque);
+void	rrr(t_deque *deque_a, t_deque *deque_b);
 
 #endif
