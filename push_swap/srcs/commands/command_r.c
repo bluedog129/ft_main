1b535900c7d22064875098424d596889375ec6ad
Hyojong Choi <bluedog129@gmail.com>
2023-03-07 11:51:15 +0900

    fix(commands) : commands print add

--- push_swap/srcs/commands/command_r.c
+++ push_swap/srcs/commands/command_r.c
@@ -6,7 +6,7 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 21:16:29 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/03/02 21:45:23 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/03/07 11:49:00 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,31 @@
 
 void	ra(t_deque *deque)
 {
+	ft_printf("ra\n");
 	push_back(deque, pop_front(deque));
 }
 
 void	rb(t_deque *deque)
 {
+	ft_printf("rb\n");
 	push_back(deque, pop_front(deque));
 }
 
 void	rr(t_deque *deque_a, t_deque *deque_b)
 {
+	ft_printf("rr\n");
 	ra(deque_a);
 	rb(deque_b);
 }
 
 void	rra(t_deque *deque)
 {
+	ft_printf("rra\n");
 	push_front(deque, pop_back(deque));
 }
 
 void	rrb(t_deque *deque)
 {
+	ft_printf("rrb\n");
 	push_front(deque, pop_back(deque));
 }
--- push_swap/srcs/commands/command_s_p.c
+++ push_swap/srcs/commands/command_s_p.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   commands.c                                         :+:      :+:    :+:   */
+/*   command_s_p.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 21:47:58 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/03/02 21:12:11 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/03/07 11:50:12 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	sa(t_deque *deque)
 {
+	ft_printf("sa\n");
 	int	temp;
 
 	if (is_empty(deque) || deque->front == deque->rear)
@@ -25,6 +26,7 @@ void	sa(t_deque *deque)
 
 void	sb(t_deque *deque)
 {
+	ft_printf("sb\n");
 	int	temp;
 
 	if (is_empty(deque) || deque->front == deque->rear)
@@ -36,12 +38,14 @@ void	sb(t_deque *deque)
 
 void	ss(t_deque *deque_a, t_deque *deque_b)
 {
+	ft_printf("ss\n");
 	sa(deque_a);
 	sb(deque_b);
 }
 
 void	pa(t_deque *deque_a, t_deque *deque_b)
 {
+	ft_printf("pa\n");
 	if (is_empty(deque_b))
 		return ;
 	push_front(deque_a, pop_front(deque_b));
@@ -49,6 +53,7 @@ void	pa(t_deque *deque_a, t_deque *deque_b)
 
 void	pb(t_deque *deque_a, t_deque *deque_b)
 {
+	ft_printf("pb\n");
 	if (is_empty(deque_a))
 		return ;
 	push_front(deque_b, pop_front(deque_a));
