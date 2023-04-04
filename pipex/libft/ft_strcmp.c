2f60da033356707dbb3d53f56d183240c9773e16
Hyojong Choi <bluedog129@gmail.com>
2023-04-04 12:34:10 +0900

    fix(libft) : strcmp null

--- pipex/libft/ft_strcmp.c
+++ pipex/libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 18:00:58 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/02/28 18:02:25 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/04/04 12:28:57 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 	unsigned char	*temp_s1;
 	unsigned char	*temp_s2;
 
+	if (s1 == NULL || s2 == NULL)
+		return 0;
 	temp_s1 = (unsigned char *)s1;
 	temp_s2 = (unsigned char *)s2;
 	while ((*temp_s1 != '\0' || *temp_s2 != '\0'))
