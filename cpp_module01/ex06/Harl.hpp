9671e81ad3022e74dc8938bbad147ae96e516e5f
Hyojong Choi <bluedog129@gmail.com>
2023-08-04 17:30:01 +0900

    feat : ex06

---
+++ cpp_module01/ex06/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 15:32:47 by hyojocho          #+#    #+#             */
+/*   Updated: 2023/08/04 17:01:50 by hyojocho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		Harl();
+		void complain(std::string level);
+		typedef void (Harl::*FuncPtr)(void);
+		FuncPtr funcPtr[4];
+};
+
+#endif
