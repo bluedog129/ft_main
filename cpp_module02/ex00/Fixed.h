c0336080bc6c3f095db3a2e77c6eb84563301cbc
Hyojong Choi <bluedog129@gmail.com>
2023-08-04 18:49:41 +0900

    feat : cpp02 setting

---
+++ cpp_module02/ex00/Fixed.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 18:37:03 by hyojocho          #+#    #+#             */
+/*   Updated: 2023/08/04 18:42:09 by hyojocho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+class Fixed
+{
+private:
+	int					value;
+	static const int	fractionalBits = 8;
+
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+};
+
+#endif
